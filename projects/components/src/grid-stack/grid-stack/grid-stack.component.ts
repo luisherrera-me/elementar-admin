ef99d62fec3d43659ae0639cce5a8f421b667556
Pavel Salauyou <pavel.salauyou@gmail.com>
2024-10-20 19:40:22 +0200

    add gridstack component

--- projects/components/src/grid-stack/grid-stack/grid-stack.component.ts
+++ projects/components/src/grid-stack/grid-stack/grid-stack.component.ts
@@ -1,7 +1,18 @@
-import { Component, contentChildren, effect, inject, input, Renderer2, TemplateRef } from '@angular/core';
+import {
+  AfterContentInit,
+  afterNextRender, AfterViewChecked,
+  Component,
+  contentChildren,
+  effect,
+  ElementRef,
+  inject,
+  input, PLATFORM_ID,
+  Renderer2,
+  TemplateRef
+} from '@angular/core';
 import { GridStackItemDefDirective } from '../grid-stack-item-def.directive';
 import { GridStackItem } from '../types';
-import { NgTemplateOutlet } from '@angular/common';
+import { isPlatformServer, NgTemplateOutlet } from '@angular/common';
 import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -19,8 +30,10 @@ import { CdkDrag } from '@angular/cdk/drag-drop';
     '[class.is-dragging-active]': '_isDraggingActive',
   }
 })
-export class GridStackComponent {
+export class GridStackComponent implements AfterViewChecked, AfterContentInit {
+  private _elementRef = inject(ElementRef);
   private _renderer = inject(Renderer2);
+  private _platformId = inject(PLATFORM_ID);
   private _defs = contentChildren(GridStackItemDefDirective);
   items = input.required<GridStackItem[]>();
 
@@ -34,11 +47,26 @@ export class GridStackComponent {
     y: 0
   };
   protected _isDraggingActive = false;
+  private _rootElementWidth = 0;
+  private _rootElementHeight = 0;
 
   constructor() {
     effect(() => {
       this._items = this.items();
+      this._calculateRootElementHeight();
     });
+  }
+
+  ngAfterViewChecked() {
+    if (isPlatformServer(this._platformId)) {
+      return;
+    }
+
+    const width = this._elementRef.nativeElement.getBoundingClientRect().width;
+
+    if (width !== this._rootElementWidth) {
+      this._rootElementWidth = width;
+    }
   }
 
   ngAfterContentInit() {
@@ -57,7 +85,6 @@ export class GridStackComponent {
   }
 
   onDragStarted(event: any, item: GridStackItem): void {
-    console.log(event);
     const dragElement = event.source.element.nativeElement;
     const dimensions = dragElement.getBoundingClientRect();
     this._renderer.setStyle(dragElement, 'max-width', dimensions.width + 'px');
@@ -85,5 +112,26 @@ export class GridStackComponent {
     this._renderer.removeStyle(dragElement, 'top');
     this._renderer.removeStyle(dragElement, 'left');
     dragRef.reset();
+  }
+
+  onDragMoved(event: any, item: GridStackItem, dragRef: CdkDrag): void {
+    console.log(this._rootElementWidth);
+    console.log(this._rootElementHeight);
+  }
+
+  private _calculateRootElementHeight(): void {
+    let y = 0;
+    let h = 0;
+    this.items().forEach((item: GridStackItem) => {
+      if (item.y > y) {
+        y = item.y;
+      }
+
+      if (item.h > h) {
+        h = item.h;
+      }
+    });
+    this._rootElementHeight = (y + h) * 100;
+    this._renderer.setStyle(this._elementRef.nativeElement, 'height', this._rootElementHeight + 'px');
   }
 }
